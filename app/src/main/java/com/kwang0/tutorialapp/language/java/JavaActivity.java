f5c56c13f103d55fb0fd7e11fbef990fdb11aadb
KWANGYOUNGCHOI-butake <kwangyoung.choi@butake.com>
2020-05-14 20:25:08 +0900

    features/cky/20.05.14 Update Language Activity

--- app/src/main/java/com/kwang0/tutorialapp/language/java/JavaActivity.java
+++ app/src/main/java/com/kwang0/tutorialapp/language/java/JavaActivity.java
@@ -3,6 +3,7 @@ package com.kwang0.tutorialapp.language.java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.kwang0.tutorialapp.R;
 
@@ -12,5 +13,12 @@ public class JavaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java);
+
+        TextView tv = (TextView) findViewById(R.id.java_tv);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hi, Java!").append("\n");
+
+        tv.setText(sb.toString());
     }
 }
--- app/src/main/java/com/kwang0/tutorialapp/language/kotlin/KotlinActivity.kt
+++ app/src/main/java/com/kwang0/tutorialapp/language/kotlin/KotlinActivity.kt
@@ -1,7 +1,8 @@
 package com.kwang0.tutorialapp.language.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.kwang0.tutorialapp.R
 
 class KotlinActivity : AppCompatActivity() {
@@ -9,5 +10,12 @@ class KotlinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin)
+
+        val tv : TextView = findViewById(R.id.kotlin_tv);
+
+        val sb = StringBuilder();
+        sb.append("Hi,Kotlin!").appendln();
+
+        tv.text = sb.toString()
     }
 }
