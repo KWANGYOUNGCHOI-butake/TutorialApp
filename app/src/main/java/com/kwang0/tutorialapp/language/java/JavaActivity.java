5d912a480383270e0ddb40356dc3e40a72534026
KWANGYOUNGCHOI-butake <kwangyoung.choi@butake.com>
2020-05-15 00:28:42 +0900

    features/cky/20.05.14 Update Language Activity

--- app/src/main/java/com/kwang0/tutorialapp/language/java/JavaActivity.java
+++ app/src/main/java/com/kwang0/tutorialapp/language/java/JavaActivity.java
@@ -3,6 +3,7 @@ package com.kwang0.tutorialapp.language.java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
 import android.widget.TextView;
 
 import com.kwang0.tutorialapp.R;
@@ -18,6 +19,72 @@ public class JavaActivity extends AppCompatActivity {
 
         StringBuilder sb = new StringBuilder();
         sb.append("Hi, Java!").append("\n");
+
+        int a1 = 10, a2 = 20;
+//        int max = a1 > a2 ? a : a2;
+//        int max = Math.max(a1, a2);
+        int max = 0;
+        if(a1 > a2) {
+            max = a1;
+        } else {
+            max = a2;
+        }
+        sb.append("\n").append("if").append("\n");
+        sb.append("a1 = 10, a2 = 20, max = " + max).append("\n");
+
+
+        int b1 = 10;
+        sb.append("\n").append("if range").append("\n");
+        sb.append("b1 = 10, b1 <= 20 && b1 >= 0").append("\n");
+        if(b1 >= 0 && b1 <= 20) {
+            sb.append("true").append("\n");
+        } else {
+            sb.append("false").append("\n");
+        }
+
+
+        int x = 0;
+        sb.append("\n").append("as like when").append("\n");
+        sb.append("x = 0, if x = 1, else if x = 2, else x is neither 1 nor 2").append("\n");
+        if(x == 1) {
+            sb.append("x = 1").append("\n");
+        } else if(x == 2) {
+            sb.append("x = 2").append("\n");
+        } else {
+            sb.append("x is neither 1 nor 2").append("\n");
+        }
+
+
+        sb.append("\n").append("for increase").append("\n");
+        for(int i = 1; i <= 3; i++) {
+            sb.append(i).append(" ");
+        }
+        sb.append("\n");
+
+
+        sb.append("\n").append("for decrease").append("\n");
+        for(int i = 3; i >= 1; i--) {
+            sb.append(i).append(" ");
+        }
+        sb.append("\n");
+
+
+        sb.append("\n").append("for stemp").append("\n");
+        for(int i = 0; i <= 6; i += 2) {
+            sb.append(i).append(" ");
+        }
+        sb.append("\n");
+
+
+        int y = 5;
+        sb.append("\n").append("while").append("\n");
+        sb.append("y = 5, while y > 0 y--").append("\n");
+        while(y > 0) {
+            sb.append(y).append(" ");
+            y--;
+        }
+
+
 
         tv.setText(sb.toString());
     }
--- app/src/main/java/com/kwang0/tutorialapp/language/kotlin/KotlinActivity.kt
+++ app/src/main/java/com/kwang0/tutorialapp/language/kotlin/KotlinActivity.kt
@@ -11,10 +11,74 @@ class KotlinActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin)
 
-        val tv : TextView = findViewById(R.id.kotlin_tv);
+        val tv : TextView = findViewById(R.id.kotlin_tv)
 
-        val sb = StringBuilder();
-        sb.append("Hi,Kotlin!").appendln();
+        val sb = StringBuilder()
+        sb.append("Hi, Kotlin!").appendln()
+
+
+        val e0: Int = 10000
+        sb.appendln().append("===").appendln()
+        val e1: Int? = e0
+        val e2: Int? = e0
+        sb.append("e0 = 10000, e1 = e0, e2 = e0").appendln()
+        sb.append(e1 === e2).appendln()
+
+        sb.appendln().append("==").appendln()
+        sb.append("e0 = 10000, e1 = e0, e2 = e0").appendln()
+        sb.append(e1 == e2).appendln()
+
+
+        val a1 = 10
+        val a2 = 20
+        val max = if(a1 > a2) a1 else a2
+        sb.appendln().append("if").appendln()
+        sb.append("a1 = 10, a2 = 20, max = " + max)
+
+
+        val b1 = 10
+        sb.appendln().append("if range").appendln()
+        sb.append("b1 = 10, b1 <= 20 && b1 >= 0").appendln()
+        if(b1 in 0..20) sb.append("true").appendln()
+        else sb.append("false").appendln()
+
+
+
+        val x = 0
+        sb.appendln().append("when").appendln()
+        sb.append("x = 0, if x = 1, else if x = 2, else x is neither 1 nor 2").appendln()
+        when(x) {
+            1 -> sb.append("x = 1").appendln()
+            2 -> sb.append("x = 2").appendln()
+            else -> sb.append("x is neither 1 nor 2").appendln()
+        }
+
+
+        sb.appendln().append("for increase").appendln()
+        for(i in 1..3) sb.append(i).append(" ")
+        sb.appendln()
+
+
+        sb.appendln().append("for decrease").appendln()
+        for(i in 3 downTo 1) sb.append(i).append(" ")
+        sb.appendln()
+
+
+        sb.appendln().append("for stemp").appendln()
+        for(i in 0..6 step 2) sb.append(i).append(" ")
+        sb.appendln()
+
+
+        var y = 5
+        sb.appendln().append("while").appendln()
+        sb.append("y = 10, while y > 0 y--").appendln()
+        while(y > 0) {
+            sb.append(y).append(" ")
+            y--
+        }
+        sb.appendln()
+
+
 
         tv.text = sb.toString()
     }
